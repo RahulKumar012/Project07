faef4366a204d4504a7e7aa8719e402f815bf0d8
RahulKumar012 <99016806+RahulKumar012@users.noreply.github.com>
2022-02-11 19:51:06 +0530

    Add files via upload

---
+++ Delete_User/src/Main.java
@@ -0,0 +1,26 @@
+
+
+import com.spring.entity.*;
+import com.spring.dao.*;
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+		Logger log = Logger.getLogger("org.hibernate");
+		log.setLevel(Level.OFF);
+		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+		
+		
+		
+	}
+
+}
---
+++ Delete_User/src/com/spring/dao/OwnerDAO.java
@@ -0,0 +1,33 @@
+package com.spring.dao;
+
+import com.spring.entity.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+public class OwnerDAO {
+    
+	
+	public List<Owner> getAllOwners(){
+		return null;
+		
+	}
+	
+	public String getOwnerById(int id) {
+		
+		
+	}
+	public void deleteOwner(int id){
+
+	}
+	
+	
+
+}
